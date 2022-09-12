54283c1ed43a04206c613e3b64282b3ae794e527
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-09-12 17:52:17 -0400

    Created a new component file - 'AddEntryForm' - this allows for new weight/date entries to be added via form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '09-12-2022'}])
   
+  function addNewEntry(entry){
+    
+    let tempEntries = [entry, ...entries]
+
+    setEntries(tempEntries);
+  }
   
   return (
     <div>
       <DisplayEntries parentEntries = {entries}/>
+
+      <AddEntryForm addNewEntryProperty = {addNewEntry}/>
     </div>
   );
 }
